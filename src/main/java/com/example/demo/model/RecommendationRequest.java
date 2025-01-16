c83cb0c96fc38bcb8c0306f4166c8919c8a351c6
Claire Yu <cmy42@cornell.edu>
2025-01-15 20:21:01 -0800

    Update pom.xml dependencies and refactor RecommendationRequest model
    - Updated Lombok dependency in pom.xml to version 1.18.24 and set its scope to provided.
    - Removed unused Lombok annotations (@NoArgsConstructor, @AllArgsConstructor) from RecommendationRequest class to simplify the model structure.

--- src/main/java/com/example/demo/model/RecommendationRequest.java
+++ src/main/java/com/example/demo/model/RecommendationRequest.java
@@ -2,12 +2,8 @@ package com.example.demo.model;
 
 import java.util.List;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class RecommendationRequest {
     private List<Song> inputSongs;
 } 
